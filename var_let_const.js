bfe8388e146288c76f40480eae561ef76215d29d
Yatharth <vasub0723@gmail.com>
2025-06-30 02:03:16 +0530

    we're so back

---
+++ var_let_const.js
@@ -0,0 +1,4 @@
+// var was used in the es5 version of javascript
+// let and const introduced in the es6 version which was newer. COnst is well , in its name ,constant
+//primary difference between var and let is that var is function scope,d this means that I can initialize var anywhere in my code and use it anywhere , so within a function , no concept of local/global
+//let works like noraml variables in other programming languages 
